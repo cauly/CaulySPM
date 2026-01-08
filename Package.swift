132f9eaed5d13474fd00602d90e7aeadd04b1062
김주현 <juhyunk2@fsn.co.kr>
2026-01-08 16:03:02 +0900

    3.1.22 업데이트

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version: 5.8
+import PackageDescription
+
+let package = Package(
+    name: "CaulySPM",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "CaulySDK",
+            targets: ["CaulySDKResources"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "CaulySDK",
+            path: "CaulySDK.xcframework"
+        ),
+
+        .target(
+            name: "CaulySDKResources",
+            dependencies: [
+                .target(name: "CaulySDK")
+            ],
+            linkerSettings: [
+                .linkedFramework("AVKit"),
+                .linkedFramework("UIKit"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("SystemConfiguration"),
+                .linkedFramework("MediaPlayer"),
+                .linkedFramework("CFNetwork"),
+                .linkedFramework("EventKit"),
+                .linkedFramework("AdSupport"),
+                .linkedFramework("MessageUI",),
+            ]
+        )
+    ]
+)
